fb5f52d60dd5114ec137383fda24a271e2b2e61a
kyuriiii <kyuri99@naver.com>
2025-10-27 10:53:12 +0900

    Arduino Lesson Prepare #2

--- material/Arduino/03_02_pressure_Lesson/03_02_pressure_Lesson.ino
+++ material/Arduino/03_02_pressure_Lesson/03_02_pressure_Lesson.ino
@@ -1,44 +1,68 @@
-#include <Wire.h>
 #include <SPI.h>
-#include <Adafruit_BMP280.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>  // ← BME280이면 위 라이브러리/객체만 BME로 바꾸기
 
-#define BMP_SCK  8
-#define BMP_MISO 9
-#define BMP_MOSI 10
-#define BMP_CS   11
+#define PIN_SCK  13   // SCL
+#define PIN_MOSI 11   // SDA
+#define PIN_CS   10   // CSB
+#define PIN_MISO 12   // SDO
 
-//Adafruit_BMP280 bmp; // I2C
-//Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
-Adafruit_BMP280 bmp(BMP_MOSI, BMP_MISO, BMP_CS,  BMP_SCK);
+Adafruit_BMP280 bmp(PIN_CS, PIN_MOSI, PIN_MISO, PIN_SCK);
 
 void setup() {
   Serial.begin(9600);
-  while ( !Serial ) delay(100);   // wait for native usb
-  Serial.println(F("BMP280 test"));
-  bmp.begin();
-  
-
-  /* Default settings from datasheet. */
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+  if (!bmp.begin()) {
+    Serial.println(F("BMP280 인식 실패"));
+    while (1);
+  }
 }
 
 void loop() {
-    Serial.print(F("Temperature = "));
-    Serial.print(bmp.readTemperature());
-    Serial.println(" *C");
-
-    Serial.print(F("Pressure = "));
-    Serial.print(bmp.readPressure());
-    Serial.println(" Pa");
-
-    Serial.print(F("Approx altitude = "));
-    Serial.print(bmp.readAltitude(1008.5)); /* Adjusted to local forecast! */
-    Serial.println(" m");
-
-    Serial.println();
-    delay(2000);
+  Serial.println("======================");
+  Serial.print("온도 = "); Serial.print(bmp.readTemperature()); Serial.println(" °C");
+  Serial.print("기압 = "); Serial.print(bmp.readPressure());    Serial.println(" Pa");
+  Serial.print("고도 = "); Serial.print(bmp.readAltitude(1013.25)); Serial.println(" m");
+  delay(1000);
 }
+
+
+/*
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BMP280.h>
+
+#define BMP_CS   10  // CSB
+#define BMP_MOSI 11  // SDI
+#define BMP_MISO 12  // SDO
+#define BMP_SCK  13  // SCK
+
+Adafruit_BMP280 bmp(BMP_CS);
+
+void setup() {
+  Serial.begin(9600);
+
+  if(!bmp.begin()) {
+    Serial.println(F("센서가 인식되지 않습니다. 연결 상태를 확인해주세요."));
+    while(1);
+  }
+}
+
+void loop() {
+  delay(100);
+  Serial.println("======================");
+
+  Serial.print(F("온도 = "));
+  Serial.print(bmp.readTemperature()); // 온도 측정
+  Serial.print("°C");
+
+  Serial.print(F("기압 = "));
+  Serial.print(bmp.readPressure()); // 기압 측정
+  Serial.print("°C");
+
+  Serial.print(F("고도 = "));
+  Serial.print(bmp.readAltitude(1022.5)); // 고도 측정
+  Serial.print("°C");
+
+  delay(1000);
+}
+*/
--- material/Arduino/03_03_ultrasound_Practice/03_03_ultrasound_Practice.ino
+++ material/Arduino/03_03_ultrasound_Practice/03_03_ultrasound_Practice.ino
@@ -1,7 +1,7 @@
 #define TRIG 9
 #define ECHO 8 
-#define PIEZO 13
-#define LED 2
+#define PIEZO 12
+#define LED 13
 
 void setup() {
   Serial.begin(9600);
@@ -15,25 +15,17 @@ void loop() {
   digitalWrite(PIEZO, LOW);
   digitalWrite(LED, LOW);
 
-  long duration, distance;
-  digitalWrite(TRIG, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
+  digitalWrite(TRIG,HIGH);
+  delay(10);
+  digitalWrite(TRIG,LOW);
 
-  duration = pulseIn (ECHO, HIGH); 
-  //물체에 반사되어돌아온 초음파의 시간을 변수에 저장합니다.
-  //34000*초음파가 물체로 부터 반사되어 돌아오는시간 /1000000 / 2(왕복값이아니라 편도값이기때문에 나누기2를 해줍니다.)
-  //초음파센서의 거리값이 위 계산값과 동일하게 Cm로 환산되는 계산공식 입니다. 수식이 간단해지도록 적용했습니다.
-  distance = duration * 17 / 1000; //PC모니터로 초음파 거리값을 확인 하는 코드 입니다.
-
-  Serial.println(duration ); 
-  Serial.print("\nDIstance : ");
+  float duration = pulseIn(ECHO,HIGH);
+  float distance = ((34000*duration)/1000000)/2;
   Serial.print(distance);
-  Serial.println(" Cm");
+  Serial.println("cm");
+  delay(100);
 
-  if (distance > 0){
+  if (distance < 100){
     Serial.println("PIEZO - LED");
     digitalWrite(PIEZO, HIGH);
     digitalWrite(LED, HIGH);
